ef4f947aa4f9ca972ac4ed77424146f989912562
Nubezzi <patenur@hotmail.com>
2021-11-25 02:03:57 +0200

    1.5

--- kurssitiedot/src/App.js
+++ kurssitiedot/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Header = (props) => {
+  console.log(props)
   return(
     <div>
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   </div>
   )
 }
@@ -27,7 +28,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props)
+  //console.log(props)
   return(
     <div>
     <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
@@ -36,27 +37,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 
