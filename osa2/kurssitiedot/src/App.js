a4a657460c803ec2388e328baad7e005794e87d6
Nubezzi <patenur@hotmail.com>
2022-07-11 16:35:55 +0300

    2.1 tehty

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,20 +1,38 @@
 import React from 'react'
 
-const Header = (props) => {
+const Course = (props) => {
   return(
     <div>
-    <h1>{props.course}</h1>
-  </div>
-  )
-}
-const Content = (props) => {
-  return(
-    <div>
-    <p>{props.desc} {props.exercises}</p> 
+    <Header course={props.course}/>
+    <Content course={props.course.parts}/>
   </div>
   )
 }
 
+const Header = (props) => {
+  console.log(props)
+  return(
+    <h1>{props.course.name}</h1>
+  )
+}
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+    {props.course.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return(
+    <div>
+    <p>{props.part.name} {props.part.exercises}</p> 
+  </div>
+  )
+}
+/*
 const Total = (props) => {
   return(
     <div>
@@ -22,23 +40,34 @@ const Total = (props) => {
   </div>
   )
 }
-
+*/
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content desc={part1} exercises={exercises1} />
-      <Content desc={part2} exercises={exercises2} />
-      <Content desc={part3} exercises={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Course course={course} />
     </div>
   )
 }
