f7ea76e79f799822198a6cfc9e8bd0e388ddc351
Nubezzi <patenur@hotmail.com>
2022-07-11 20:18:06 +0300

    2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,52 @@
 import { useState } from 'react'
 
+const Filter = ({value, change}) => {
+  return (
+    <div>
+      filter the search: <input
+          value={value}
+          onChange={change}
+          />
+    </div>
+  )
+}
+
+const Adder = ({addPerson, newName, handleChangeName, newNumber, handleChangeNum}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input
+          value={newName}
+          onChange={handleChangeName}
+          />
+      </div>
+      <div>
+        number: <input 
+          value={newNumber}
+          onChange={handleChangeNum}
+        />
+      </div>
+          <button type="submit">add</button>
+    </form>
+  )
+}
+
+const ContactBook = ({ whatToShow }) => {
+  return (
+    <ul>
+        {whatToShow.map(person =>
+          <Person key={person.name} person={person}/>
+        )}
+    </ul>
+  )
+}
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}, {person.number}</p>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -16,8 +63,6 @@ const App = () => {
     const noteObject = {
       name: newName,
       number: newNumber,
-      //date: new Date().toISOString(),
-      //id: persons.length + 1,
     }
 
     if(persons.filter(per => per.name === newName).length > 0){
@@ -27,62 +72,46 @@ const App = () => {
       setNewName('')
       setNewNumber('')
     }
-  
-    
   }
 
-  const Person = ({ person }) => {
-    return (
-      <p>{person.name}, {person.number}</p>
-    )
-  }
+  
 
   const handleChangeName = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
   const handleChangeNum = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
   const handleChangeSearch = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewSearch(event.target.value)
   }
 
   const whatToShow = persons.filter(x => x.name.toLocaleLowerCase('no-no').includes(newSearch) == true)
 
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter the search: <input
-            value={newSearch}
-            onChange={handleChangeSearch}
-            />
-      </div>
+      <Filter 
+        value={newSearch}
+        change={handleChangeSearch}
+      />
       <h2>Add numbers</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-            value={newName}
-            onChange={handleChangeName}
-            />
-        </div>
-        <div>
-          number: <input 
-            value={newNumber}
-            onChange={handleChangeNum}
-          />
-        </div>
-            <button type="submit">add</button>
-      </form>
+      <Adder
+        addPerson={addPerson}
+        newName={newName}
+        handleChangeName= {handleChangeName}
+        newNumber= {newNumber}
+        handleChangeNum={handleChangeNum}
+      />
       
       <h2>Numbers</h2>
       <ul>
-        {whatToShow.map(person =>
-          <Person key={person.name} person={person}/>
-        )}
+        <ContactBook whatToShow={whatToShow}/>
       </ul>
     </div>
   )
