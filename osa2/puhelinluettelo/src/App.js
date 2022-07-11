f17d6e1c02bae113f890eddeb42d824806951b65
Nubezzi <patenur@hotmail.com>
2022-07-11 18:50:57 +0300

    2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0501234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -41,10 +45,23 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleChangeSearch = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value)
+  }
+
+  const whatToShow = persons.filter(x => x.name.toLocaleLowerCase('no-no').includes(newSearch) == true)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter the search: <input
+            value={newSearch}
+            onChange={handleChangeSearch}
+            />
+      </div>
+      <h2>Add numbers</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -63,8 +80,8 @@ const App = () => {
       
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />
+        {whatToShow.map(person =>
+          <Person key={person.name} person={person}/>
         )}
       </ul>
     </div>
